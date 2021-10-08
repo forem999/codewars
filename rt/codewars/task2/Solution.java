9140767b3d750e3b39eb5ab0b87bacb5ad5318b8
forem <forem999@mail.ru>
2021-10-08 23:34:58 +0300

    8.10.2021

--- rt/codewars/task2/Solution.java
+++ rt/codewars/task2/Solution.java
@@ -6,18 +6,30 @@ public class Solution {
 
     public static void main(String[] args) {
 
+
     }
 
     public static int ConvertBinaryArrayToInt(List<Integer> binary) {
         // Your Code
         int summ = 0;
 
-        for (int i : binary) {
-            summ += i;
+        for (int i = binary.size()-1; i >= 0; i--) {
+            if (binary.get(i) == 1) {
+                summ = summ + powOfTwo(binary.size() - 1 - i);
+            }
         }
-
-
         return summ;
     }
+
+    public static int powOfTwo (int pow) {
+        if (pow == 0) {
+            return 1;
+        } else {
+            return 2*powOfTwo(pow-1);
+        }
+
+    }
+
+
 
 }

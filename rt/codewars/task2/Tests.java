4ec80d8349e05e68ab01cbe30003ba4927aad434
forem <forem999@mail.ru>
2021-10-12 12:32:01 +0300

    12.10.2021

--- rt/codewars/task2/Tests.java
+++ rt/codewars/task2/Tests.java
@@ -1,9 +1,7 @@
 package rt.codewars.task2;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions.*;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 
 public class Tests {

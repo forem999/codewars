3c757006b85233a217631a52c3e9e01930574a22
forem <forem999@mail.ru>
2021-10-14 19:11:39 +0300

    09.10.2021 19:11

--- rt/codewars/task7/Solution.java
+++ rt/codewars/task7/Solution.java
@@ -3,10 +3,32 @@ package rt.codewars.task7;
 public class Solution {
 
     public static void main(String[] args) {
-
+        System.out.println(camelCase("HelloWorlD"));
     }
 
     public static String camelCase(String input) {
-        return null;
+
+        if (input == null) {
+            return "";
+        }
+
+        char[] charList;
+        charList = input.toCharArray();
+        StringBuilder result = new StringBuilder();
+        boolean first = true;
+        for (char c : charList) {
+
+            if (first) {
+                result.append(c);
+                first = false;
+                continue;
+            }
+            if ( ((int) c >= 65 ) && ( (int) c <= 90) ) {
+            result.append(" " + c);
+            } else {
+                result.append(c);
+            }
+        }
+        return result.toString();
     }
 }

963ce717871d87aa9fe125000838d8ebbb5c2dba
forem <forem999@mail.ru>
2021-10-14 12:30:23 +0300

    14.10.2021 12:30

--- rt/codewars/task2/Tests.java
+++ rt/codewars/task2/Tests.java
@@ -1,7 +1,7 @@
 package rt.codewars.task2;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 
 public class Tests {
---
+++ rt/codewars/task5/Tests.java
@@ -0,0 +1,14 @@
+package rt.codewars.task5;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Tests {
+
+    @Test
+    public void basicTest () {
+        assertEquals(2, Solution.stray(new int[]{2,1,1,1,1}));
+
+    }
+
+}

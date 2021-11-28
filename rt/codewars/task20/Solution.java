ead6bb1bfe0bb967e46a3cc63d077eef97af6e36
forem <forem999@mail.ru>
2021-11-28 14:32:56 +0300

    28.11.2021

--- rt/codewars/task20/Solution.java
+++ rt/codewars/task20/Solution.java
@@ -4,13 +4,14 @@
 package rt.codewars.task20;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Solution {
 
     public static void main(String[] args) {
 
-        stat("01|15|59, 1|47|16, 01|17|20, 1|32|34, 2|17|17");
+        stat("02|15|59, 2|47|16, 02|17|20, 2|32|34, 2|17|17, 2|22|00, 2|31|41");
 
     }
 
@@ -29,10 +30,76 @@ public class Solution {
 
         }
 
-        System.out.println(list);
+        StringBuilder sb = new StringBuilder();
 
-        return "OK";
+        // find Range
+        int max = Collections.max(list);
+        int min = Collections.min(list);
+        int Range = max-min;
 
+        sb.append("Range: ");
+        sb.append(formatStr(Range));
+    //  System.out.println(sb);
+
+
+        // find Average
+        int MoA = 0;
+        int summ = 0;
+        for (int e : list) summ += e;
+        MoA = summ / list.size();
+
+        sb.append(" Average: ");
+        sb.append(formatStr(MoA));
+    //    System.out.println(sb);
+
+        // find Median
+
+        int median = 0;
+
+        Collections.sort(list);
+
+        if (list.size()%2 > 0 ) {
+            median = list.get(list.size()/2);
+        } else {
+            median = (list.get(list.size()/2 - 1)  + list.get(list.size()/2))/2;
+        }
+
+        sb.append(" Median: ");
+        sb.append(formatStr(median));
+    //    System.out.println(sb);
+
+        return sb.toString();
+
+
+    }
+
+    public static String formatStr (int value) {
+        StringBuilder sb = new StringBuilder();
+        if (value/3600 > 9) {
+            sb.append(value/3600);
+            sb.append("|");
+        } else {
+            sb.append("0");
+            sb.append(value/3600);
+            sb.append("|");
+        }
+
+        if ((value%3600)/60 > 9 ) {
+            sb.append((value%3600)/60);
+            sb.append("|");
+        } else {
+            sb.append("0");
+            sb.append((value%3600)/60);
+            sb.append("|");
+        }
+
+        if ((value%3600)%60 > 9 ) {
+            sb.append((value%3600)%60);
+        } else {
+            sb.append("0");
+            sb.append((value%3600)%60);
+        }
+        return sb.toString();
 
     }
 

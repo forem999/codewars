128025f697fe2fc35e2197a6dbcc22fe6bc62ebd
forem <forem999@mail.ru>
2021-12-01 18:40:41 +0300

    01.12.2021

--- rt/codewars/task32/Solution.java
+++ rt/codewars/task32/Solution.java
@@ -5,12 +5,22 @@ package rt.codewars.task32;
 
 public class Solution {
 
+    public static void main(String[] args) {
+        System.out.println(maskify("4556364607935616"));
+    }
+
 
     public static String maskify(String str) {
-        char[] charArr = str.toCharArray();
-        if charArr.length < 4
 
-        throw new UnsupportedOperationException("Unimplemented");
+        StringBuilder sb = new StringBuilder(str);
+        if (sb.length() <= 4) {
+            return sb.toString();
+        } else {
+            for (int i = 0; i < sb.length() - 4; i++) {
+                sb.setCharAt(i, '#');
+            }
+            return sb.toString();
+        }
 
     }
 }

fb240c8e9a999060c295120141fda4d607926302
forem <forem999@mail.ru>
2021-12-01 08:03:45 +0300

    1.12.2021

---
+++ rt/codewars/task32/Solution.java
@@ -0,0 +1,16 @@
+// https://www.codewars.com/kata/5412509bd436bd33920011bc/train/java
+
+
+package rt.codewars.task32;
+
+public class Solution {
+
+
+    public static String maskify(String str) {
+        char[] charArr = str.toCharArray();
+        if charArr.length < 4
+
+        throw new UnsupportedOperationException("Unimplemented");
+
+    }
+}

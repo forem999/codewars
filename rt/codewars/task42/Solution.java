6ace02e1285ddbc28d5b366230985880d1c74d04
forem <forem999@mail.ru>
2022-04-22 19:27:28 +0300

    1

--- rt/codewars/task42/Solution.java
+++ rt/codewars/task42/Solution.java
@@ -1,16 +1,30 @@
 package rt.codewars.task42;
 //https://www.codewars.com/kata/55f8a9c06c018a0d6e000132/train/java
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution {
     public static void main(String[] args) {
+        System.out.println(validatePin("1234"));
 
     }
 
     public static boolean validatePin(String pin) {
         // Your code here...
-        if ((pin.length()!=4) || (pin.length()!=6)){
+       if ((pin.length()!=4) || (pin.length()!=6)){
             return false;
         }
-        return true;
+        char[] dig = {'0','1','2','3','4','5','6','7','8','9'};
+        Set chSet = new HashSet<Character>();
+        chSet.add(dig);
+        char[] chArr = pin.toCharArray();
+        boolean flag = true;
+        for (char ch : chArr) {
+            if (!chSet.contains(ch)) {
+                flag = false;
+            }
+        }
+        return flag;
     }
 }
